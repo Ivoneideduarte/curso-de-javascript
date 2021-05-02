56fed5af0f076736078f97fdd66a78c0ff767ad2
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-02 20:27:12 -0300

    Condição simples

---
+++ Módulo D - Condições em JavaScript/01 - Condições p1/ex002.js
@@ -0,0 +1,10 @@
+// console.log('O console funcionou corretamente!')
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60)
+{ // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
