a29506647723005744840e4f52d6ef13719e495c
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-02 21:05:47 -0300

    Desafio resolvido

--- Módulo D - Condições em JavaScript/01 - Condições p1/ex003.js
+++ Módulo D - Condições em JavaScript/01 - Condições p1/ex003.js
@@ -1,7 +1,7 @@
 var pais = 'EUA'
 console.log(`Vivendo em ${pais}`)
-if (pais == 'Você é Brasil') {
-    console.log('Brasileiro!')
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro!')
 } else {
     console.log('Você é Estrangeiro!')
 }
