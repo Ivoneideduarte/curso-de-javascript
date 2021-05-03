08ba6c00080bebd7287804a37c77fd70e79dc451
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-03 15:17:38 -0300

    Exercício resolvido

---
+++ Módulo D - Condições em JavaScript/02 - Condições p2/02 - Exercício/modelo/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        // Bom dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        // Boa tarde!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+}
