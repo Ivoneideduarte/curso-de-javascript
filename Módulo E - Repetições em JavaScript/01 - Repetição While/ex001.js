83c36e54427281b3b41d122d4554a62f7d22833f
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-03 21:20:55 -0300

    Repetição While

---
+++ Módulo E - Repetições em JavaScript/01 - Repetição While/ex001.js
@@ -0,0 +1,29 @@
+/*
+var c = 1
+
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+Estrutura de repetição com teste lógico no início
+
+while (condição) {
+    bloco
+}
+
+Estrutura de repetição com teste lógico no final
+
+do {
+    bloco
+} while (condição)
+
+*/
