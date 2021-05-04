69d4fc1f8e65f11fd536b7f3c6bce5833cf91529
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-04 16:15:33 -0300

    Exercício resolvido

--- Módulo E - Repetições em JavaScript/01 - Exercício/script.js
+++ Módulo E - Repetições em JavaScript/01 - Exercício/script.js
@@ -1,16 +1,33 @@
-function contar() 
-{
+function contar() {
     // Pego os valores da caixa de texto pela ID e armazeno nas variáveis
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
-    var res = document.getElementById('res')
-    // Converto os valores recebidos em formato de string e passo para Number
-    var i = Number.parseInt(inicio.value)
-    var f = Number.parseInt(fim.value)
-    var p = Number.parseInt(passo.value)
-    
-    for(var i; i <= f; i++) {
-        res.innerHTML = `Contando: <strong>${i}</strong>`
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let res = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { // Verifica se a caixa de texto está vazia
+        alert('[ERRO] O campo está vazio!')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: '
+        // Converto os valores recebidos em formato de string e passo para Number
+        let i = Number.parseInt(inicio.value)
+        let f = Number.parseInt(fim.value)
+        let p = Number.parseInt(passo.value)
+
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}` // Adicionando emojis
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
