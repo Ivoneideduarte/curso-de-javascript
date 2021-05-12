3aa216d4250f35371b2460b441261494d2147bb4
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-12 20:22:13 -0300

    Vetores

---
+++ Módulo F - Avançando os estudos em JavaScript/ex001.js
@@ -0,0 +1,9 @@
+let num = [5,8,4]
+/*
+num[3] = 6
+num.push(7) // Adiciona um elemento no vetor
+num.length  // Comprimento do vetor
+num.sort()  // Ordena os elementos dentro do vetor em ordem crescente
+*/
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
