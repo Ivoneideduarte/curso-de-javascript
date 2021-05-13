f04e9b631457fac8d5c3ea805984ff2324bddade
Ivoneide Duarte <contato.ivoneideduarte@gmail.com>
2021-05-12 21:01:25 -0300

    Vetor com for

---
+++ Módulo F - Avançando os estudos em JavaScript/ex002.js
@@ -0,0 +1,5 @@
+let valores = [8 , 1, 7, 4, 2, 9]
+
+for(let pos=0; pos < valores.length; pos++) { // Inicia na posição zero e vai até o comprimento do vetor
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
